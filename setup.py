26d80493d259b0d6e6827269ad0beb613063529d
羽 <Just4test@users.noreply.github.com>
2020-02-24 21:27:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cfn-dep2layer',
-    version = '0.1.4',
+    version = '0.1.5',
     keywords = ('aws', 'lambda', 'layer', 'dependencies'),
     description = 'Package dependencies to layer',
     url = 'https://github.com/Just4test/aws-lambda-dependencies-to-layer',
